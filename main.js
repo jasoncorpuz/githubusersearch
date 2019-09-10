d7034ff7fce18d733fda686a8cadf37c02b500d9
Jason Corpuz <jason.corpuz@live.com>
2019-09-09 17:07:08 -0700

    modified js

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ console.log('js is working')
 // fetch, handles getting data to JSON, response.ok, then run code with search
 //renders results 
 const url= 'https://api.github.com/users/'
+const key= 'e8dae55d9e83ffcacc7e2e896333ef66b0c773db'
+const options = {
+    headers: new Headers({
+        'Authorzation' : key
+    })
+};
 
 function handleSearch(){
     console.log('handle search working')
@@ -13,32 +19,29 @@ function handleSearch(){
         const search=$('.search').val();
         console.log(search);
         generateLink(search);
-    })
-    
-}
+    }) 
+};
+
+
 
 function generateLink(search){
     console.log('gen link working')
     console.log(`generate link: ${search}`)
-    let searchUrl = `${url}${search}/repos`;
+    const searchUrl =`${url}${search}/repos`;
     console.log(searchUrl);
     getInfo(searchUrl);
-}
+};
 
 function getInfo(searchUrl){
-    console.log(`${searchUrl} passed off`);
+    //console.log(`${searchUrl} passed off`);
     fetch(searchUrl)
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            } 
-        throw new Error(response.statusText);
-        })
-}
+        .then(response => response.json())
+        .then(console.log(responseJson))
+};
 
 function renderInfo(){
     console.log('renderInfo working')
-}
+};
 
 
 
